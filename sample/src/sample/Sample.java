4000b03c0bf287c7a1187fb973c906f52fa0247d
kingsu33 <2697467@naver.com>
2025-05-09 22:42:27 +0900

    3번째 푸쉬

--- sample/src/sample/Sample.java
+++ sample/src/sample/Sample.java
@@ -4,5 +4,6 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("hi");
 		System.out.println("why didnt it pushed?");
+		System.out.println("what shall i do?");
 	}
 }
